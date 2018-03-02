795052706343a82fbb610d71efee0fb83b8e5a77
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-02 20:02:39 -0300

    desfazendo alterações na biblioteca

--- minhabiblioteca.js
+++ minhabiblioteca.js
@@ -21,59 +21,59 @@ var BrazilianQnaMakerTools = (function() {
             var filteredResult = qnaMakerResult.answers.filter(function (qna) { return htmlentities.decode(qna.questions[0]) === results.response.entity; });
             var selectedQnA = filteredResult[0];
             // session.send(selectedQnA.answer);
-            session.endDialogWithResult(selectedQnA);
+            // session.endDialogWithResult(selectedQnA);
 
-            // const partesDaResposta = selectedQnA.answer.split('%');
-            // const [titulo, imagem, descricao, url] = partesDaResposta;
+            const partesDaResposta = selectedQnA.answer.split('%');
+            const [titulo, imagem, descricao, url] = partesDaResposta;
         
-            // var card4 = ()=>{
-            //     const card  = new builder.HeroCard(session)
-            //         .title(titulo)
-            //         .images([builder.CardImage.create(session,imagem.trim())])
-            //         .text(descricao)
-            //         .buttons([ builder.CardAction.openUrl(session, url.trim(), 'mande um email')]);
-            //     const retorno = new builder.Message(session).addAttachment(card);
-            //     session.send(retorno);
-            //     session.endDialogue();
-            // };
+            var card4 = ()=>{
+                const card  = new builder.HeroCard(session)
+                    .title(titulo)
+                    .images([builder.CardImage.create(session,imagem.trim())])
+                    .text(descricao)
+                    .buttons([ builder.CardAction.openUrl(session, url.trim(), 'mande um email')]);
+                const retorno = new builder.Message(session).addAttachment(card);
+                session.send(retorno);
+                session.endDialogue();
+            };
         
-            // var card3 = ()=>{
-            //     const card  = new builder.HeroCard(session)
-            //         .title(titulo)
-            //         .images([builder.CardImage.create(session,imagem.trim())])
-            //         .text(descricao);
-            //     const retorno = new builder.Message(session).addAttachment(card);
-            //     session.send(retorno);
-            //     session.endDialogue();
-            // };
+            var card3 = ()=>{
+                const card  = new builder.HeroCard(session)
+                    .title(titulo)
+                    .images([builder.CardImage.create(session,imagem.trim())])
+                    .text(descricao);
+                const retorno = new builder.Message(session).addAttachment(card);
+                session.send(retorno);
+                session.endDialogue();
+            };
         
-            // var card2 = ()=>{
-            //     const card  = new builder.HeroCard(session)
-            //     .text(descricao)
-            //     .buttons([ builder.CardAction.openUrl(session, url.trim(), 'mande um email')]);
-            //     const retorno = new builder.Message(session).addAttachment(card);
-            //     session.send(retorno);
-            //     session.endDialogue();
-            // };
+            var card2 = ()=>{
+                const card  = new builder.HeroCard(session)
+                .text(descricao)
+                .buttons([ builder.CardAction.openUrl(session, url.trim(), 'mande um email')]);
+                const retorno = new builder.Message(session).addAttachment(card);
+                session.send(retorno);
+                session.endDialogue();
+            };
         
-            // switch(partesDaResposta.length){
-            //     case 4:
-            //     card4();
-            //     break;
+            switch(partesDaResposta.length){
+                case 4:
+                card4();
+                break;
         
-            //     case 3:
-            //     card3();
-            //     break;
+                case 3:
+                card3();
+                break;
         
-            //     case 2:
-            //     card2();
-            //     break;
+                case 2:
+                card2();
+                break;
         
-            //     case 1:
-            //     session.send(selectedQnA);
-            //     session.endDialogue();
-            //     break;
-            // }
+                case 1:
+                session.send(selectedQnA);
+                session.endDialogue();
+                break;
+            }
         }
     ];
 
