52f455a25378ac0b17230fdb92909760f3bf10bc
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-15 01:00:08 -0300

    mudei server config de novo

--- app.js
+++ app.js
@@ -15,14 +15,17 @@ var minha = require('./minhabiblioteca');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
-// Create connector and listen for messages
+  
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
+// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
 /*----------------------------------------------------------------------------------------
